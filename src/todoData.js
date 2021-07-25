3a407443a14aa2d91c04580742e8338d195dffb1
AhmedKamon <ahmedkamon3@gmail.com>
2021-07-25 11:02:07 -0700

    changes

---
+++ src/todoData.js
@@ -0,0 +1,22 @@
+export const todoData = [
+  {
+    title: "I am Jonny",
+    isComplete: false,
+    id: 1,
+  },
+  {
+    title: "I love gamming",
+    isComplete: false,
+    id: 2,
+  },
+  {
+    title: "I love T-20",
+    isComplete: false,
+    id: 3,
+  },
+  {
+    title: "Ilets go",
+    isComplete: true,
+    id: 4,
+  },
+];
